c49db45b430e443d89e27f2b594e30ab0f7fda22
moeinZ <moein459@gmail.com>
2024-03-08 10:19:11 +0330

    feat(app): add go html template

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
-	app := fiber.New()
+	engine := html.New("../web/views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layouts/main",
+	})
+
+	app.Get("/hello-world", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.Render("index", fiber.Map{
+			"World": "World!",
+		})
 	})
 
 	app.Listen(":3000")
